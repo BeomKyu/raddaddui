ec72d5b391f0c8c30f7c8760719e8788cab18e9e
RiceMan <49937770+RiceMan98@users.noreply.github.com>
2022-04-16 18:29:46 +0900

    aaa

--- app/src/main/java/com/example/myapplication/UserActivity.java
+++ app/src/main/java/com/example/myapplication/UserActivity.java
@@ -2,15 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        Button Revokebtn = (Button) findViewById(R.id.revoke_btn);
 
+        Revokebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                revokeAccess();
+                Intent intent = new Intent(getApplicationContext(), SigninActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void revokeAccess(){
+        mAuth.getCurrentUser().delete();
     }
 }
